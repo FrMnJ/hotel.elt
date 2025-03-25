042b6dbdc8577e6ae9b6ed4f08706fbd77e3c541
Luis Alejandro Sánchez Brizio <luis.sanchez9105@alumnos.udg.mx>
2025-03-25 11:36:39 -0600

    Update json_load_command.py

--- etl/json_load_command.py
+++ etl/json_load_command.py
@@ -1,19 +1,21 @@
 import pandas as pd
-from command import Command
 import os
+from command import Command
 
-class JsonLoad(Command):
-    def __init__(self, file_path, logger):
+class JsonLoadCommand(Command):
+    def __init__(self, logger, file_path):
         super().__init__(logger)
         self.file_path = file_path
 
     def execute(self, data: pd.DataFrame):
-        try: 
-            data.to_json(self.file_path)
+        try:
+            data.to_json(self.file_path, orient="records", indent=4)
+            self.logger.write_line(f"Datos guardados en formato JSON en: {self.file_path}")
         except Exception as e:
-            self.logger.write_line(e)
+            self.logger.write_line(f"Error al guardar en JSON: {e}")
             self.undo()
 
     def undo(self):
-        if os.file.exists(self.file_path):
+        if os.path.exists(self.file_path):
             os.remove(self.file_path)
+            self.logger.write_line(f"Archivo JSON eliminado: {self.file_path}")
