830b732133245f51d55620d979655ce2c4da13bf
Luis Alejandro Sánchez Brizio <luis.sanchez9105@alumnos.udg.mx>
2025-03-25 11:37:56 -0600

    Update postgres_load_command.py

--- etl/postgres_load_command.py
+++ etl/postgres_load_command.py
@@ -1,19 +1,21 @@
 import pandas as pd
-import psycopg2 as pg
-
-from etl.command import Command
+from sqlalchemy import create_engine
+from command import Command
 
 class PostgresLoadCommand(Command):
-    def __init__(self, conn, logger):
+    def __init__(self, conn_string, table_name, logger):
         super().__init__(logger)
-        self.conn = conn
+        self.conn_string = conn_string
+        self.table_name = table_name
 
     def execute(self, data: pd.DataFrame):
         try:
-            pd.to_sql(data, self.conn, if_exists='replace')
+            engine = create_engine(self.conn_string)
+            data.to_sql(self.table_name, engine, if_exists="replace", index=False)
+            self.logger.write_line(f"Datos guardados en la tabla '{self.table_name}' de PostgreSQL.")
         except Exception as e:
-            self.logger.write_line(e)
+            self.logger.write_line(f"Error al guardar en PostgreSQL: {e}")
             self.undo()
 
     def undo(self):
-        self.conn.rollback()
+        self.logger.write_line("No se puede deshacer la operación en PostgreSQL.")
